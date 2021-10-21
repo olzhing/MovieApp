fc62cea4c8ad73c98104b9b8118bdfbb2268c03f
snezihoglu <64955154+snezihoglu@users.noreply.github.com>
2021-10-21 13:43:01 +0600

    Add files via upload

---
+++ lib/screens/dashboard_screen.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+
+import '../model/movie.dart';
+
+import '../widget/top_rated_list_item.dart';
+import '../widget/vertical_list_item.dart';
+import '../widget/horizontal_list_item.dart';
+
+class DashboardScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('PopCorn'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          ),
+          TextButton (
+            onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const LoginScreen()),
+                );
+            },
+          CircleAvatar(
+            
+            radius: 21,
+            backgroundImage: NetworkImage(
+              'https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg',
+            ),
+            
+          ),
+          ),
+          
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    'Recommended',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  FlatButton(
+                    child: Text('View All'),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              height: 280,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: movieList.length,
+                itemBuilder: (ctx, i) => HorizontalListItem(i),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    'Best of 2019',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  FlatButton(
+                    child: Text('View All'),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              height: 500,
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: ListView.builder(
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: bestMovieList.length,
+                itemBuilder: (ctx, i) => VerticalListItem(i),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    'Top Rated Movies',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  FlatButton(
+                    child: Text('View All'),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              height: 280,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: topRatedMovieList.length,
+                itemBuilder: (ctx, i) => TopRatedListItem(i),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
